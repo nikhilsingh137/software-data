839b83c9d1f2ad767d301cbbf6e8ed3b7a8efd7d
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-07-04 03:29:36 +0530

    update file

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
-let sideNav = document.getElementById("sideNav");
-let lineIcon = document.getElementById("line");
-let crossIcon = document.getElementById("cross");
-function toggleNav() {
-  sideNav.style.display = "block";
-  lineIcon.style.display = "none";
-  crossIcon.style.display = "block";
-}
-function toggleCross() {
-  sideNav.style.display = "none";
-  lineIcon.style.display = "block";
-  crossIcon.style.display = "none";
-}
-
-function handleResize() {
-  if (window.innerWidth > 991 || window.innerWidth < 360) {
-    sideNav.style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+  let sideNav = document.getElementById("sideNav");
+  let lineIcon = document.getElementById("line");
+  let crossIcon = document.getElementById("cross");
+  function toggleNav() {
+    sideNav.style.display = "block";
     lineIcon.style.display = "none";
-    crossIcon.style.display = "none";
-  } else {
-    lineIcon.style.display = "block";
+    crossIcon.style.display = "block";
   }
-}
+  function toggleCross() {
+    sideNav.style.display = "none";
+    lineIcon.style.display = "block";
+    crossIcon.style.display = "none";
+  }
 
-handleResize();
+  function handleResize() {
+    if (window.innerWidth > 991 || window.innerWidth < 360) {
+      sideNav.style.display = "none";
+      lineIcon.style.display = "none";
+      crossIcon.style.display = "none";
+    } else {
+      lineIcon.style.display = "block";
+    }
+  }
 
-window.addEventListener("resize", handleResize);
+  handleResize();
+
+  window.addEventListener("resize", handleResize);
+});

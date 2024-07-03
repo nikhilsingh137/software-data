5396b33011009ce8618bb4279f5bdc58591fdd04
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-07-04 04:03:54 +0530

    update file

--- js/index.js
+++ js/index.js
@@ -1,31 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let sideNav = document.getElementById("sideNav");
-  let lineIcon = document.getElementById("line");
-  let crossIcon = document.getElementById("cross");
-  function toggleNav() {
-    sideNav.style.width = "250px";
-    lineIcon.style.display = "none";
-    crossIcon.style.display = "block";
-  }
-  function toggleCross() {
+let sideNav = document.getElementById("sideNav");
+let lineIcon = document.getElementById("line");
+let crossIcon = document.getElementById("cross");
+function toggleNav() {
+  sideNav.style.width = "250px";
+  lineIcon.style.display = "none";
+  crossIcon.style.display = "block";
+}
+function toggleCross() {
+  sideNav.style.width = "0";
+  lineIcon.style.display = "block";
+  crossIcon.style.display = "none";
+}
+
+function handleResize() {
+  if (window.innerWidth > 991 || window.innerWidth < 360) {
     sideNav.style.width = "0";
-    lineIcon.style.display = "block";
+    lineIcon.style.display = "none";
     crossIcon.style.display = "none";
+  } else {
+    lineIcon.style.display = "block";
   }
+}
 
-  function handleResize() {
-    if (window.innerWidth > 991 || window.innerWidth < 360) {
-      sideNav.style.width = "0";
-      lineIcon.style.display = "none";
-      crossIcon.style.display = "none";
-    } else {
-      lineIcon.style.display = "block";
-    }
-  }
+handleResize();
 
-  handleResize();
-
-  window.addEventListener("resize", handleResize);
-  lineIcon.addEventListener("click", toggleNav);
-  crossIcon.addEventListener("click", toggleCross);
-});
+window.addEventListener("resize", handleResize);

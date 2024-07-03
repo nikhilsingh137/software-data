9c190fa7b36350b0051a79412575e7b9289c2fb3
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-07-04 03:19:45 +0530

    update file

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let sideNav = document.getElementById("sideNav");
+let lineIcon = document.getElementById("line");
+let crossIcon = document.getElementById("cross");
+function toggleNav() {
+  sideNav.style.display = "block";
+  lineIcon.style.display = "none";
+  crossIcon.style.display = "block";
+}
+function toggleCross() {
+  sideNav.style.display = "none";
+  lineIcon.style.display = "block";
+  crossIcon.style.display = "none";
+}
+
+function handleResize() {
+  if (window.innerWidth > 991 || window.innerWidth < 360) {
+    sideNav.style.display = "none";
+    lineIcon.style.display = "none";
+    crossIcon.style.display = "none";
+  } else {
+    lineIcon.style.display = "block";
+  }
+}
+
+handleResize();
+
+window.addEventListener("resize", handleResize);

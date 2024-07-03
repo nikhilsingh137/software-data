17b932041ba3ed41056599c229f080c66a4ca7cf
nikhilsingh137 <nikhilsurvanshi137@gmail.com>
2024-07-04 03:53:23 +0530

    update file

--- js/index.js
+++ js/index.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
   let lineIcon = document.getElementById("line");
   let crossIcon = document.getElementById("cross");
   function toggleNav() {
-    sideNav.style.display = "block";
+    sideNav.style.width = "250px";
     lineIcon.style.display = "none";
     crossIcon.style.display = "block";
   }
   function toggleCross() {
-    sideNav.style.display = "none";
+    sideNav.style.width = "0";
     lineIcon.style.display = "block";
     crossIcon.style.display = "none";
   }
 
   function handleResize() {
     if (window.innerWidth > 991 || window.innerWidth < 360) {
-      sideNav.style.display = "none";
+      sideNav.style.width = "0";
       lineIcon.style.display = "none";
       crossIcon.style.display = "none";
     } else {
